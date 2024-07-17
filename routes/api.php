92edb53536886b09301d6f615b37feaa05d67554
Surya <surya.nata.aardhana@gmail.com>
2024-07-17 14:30:51 +0700

    chore: add route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\FooterController;
 use App\Http\Controllers\home\RecentProjectsController;
+use App\Http\Controllers\home\SkillController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/home/recent-projects', [RecentProjectsController::class, 'index'])->name('home');
+Route::get('/home/recent-projects', [RecentProjectsController::class, 'index'])->name('home.recent-projects');
+Route::get('/home/skills', [SkillController::class, 'index'])->name('home.skills');
+Route::get('/footer', [FooterController::class, 'index'])->name('footer');
